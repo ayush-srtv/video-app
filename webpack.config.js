88d1e8bc9e53e52f37e309482fa4898bee48fd5b
Ayush Srivastava <ayushs.1992@gmail.com>
2023-06-05 18:39:45 +0000

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = (env, argv) => {
   const webpackConfig = {
     entry: {
       /* [string | object | array] */
-      client: "./client/client.js"
+      client: "./client/client.js",
       // webpack will take the files from ./src/index.
     },
     // defaults to ./src
@@ -16,21 +16,22 @@ module.exports = (env, argv) => {
 
     output: {
       path: join(__dirname, "/public"),
-      filename: "client.js"
+      filename: "client.js",
     },
     // and outputs into /dist as client.js.
 
     devServer: {
       compress: true,
       port: 3000,
+      disableHostCheck: true,
       proxy: {
-        "/api": "http://localhost:3001"
-      }
+        "/api": "http://localhost:3001",
+      },
     },
 
     // adding .ts and .tsx to resolve.extensions will help babel look for .ts(x) files to transpile.
     resolve: {
-      extensions: [".ts", ".tsx", ".js", ".jsx", ".scss", ".svg"]
+      extensions: [".ts", ".tsx", ".js", ".jsx", ".scss", ".svg"],
     },
 
     module: {
@@ -38,30 +39,30 @@ module.exports = (env, argv) => {
         {
           test: /\.(ts|js)x?$/,
           use: {
-            loader: "babel-loader"
+            loader: "babel-loader",
           },
           include: [resolve(__dirname, "client")],
-          exclude: [resolve(__dirname, "node_modules")]
-        }
+          exclude: [resolve(__dirname, "node_modules")],
+        },
         // we use babel-loader to load our jsx and tsx files.
-      ]
+      ],
     },
 
     node: {
-      fs: "empty"
+      fs: "empty",
     },
 
     stats: {
-      children: false
+      children: false,
     },
 
     plugins: [
       new DefinePlugin({}),
       new HtmlWebpackPlugin({
         title: "LUNCHI • I'm starvinggg!!!",
-        template: join(__dirname, "/client/template/template.html")
-      })
-    ]
+        template: join(__dirname, "/client/template/template.html"),
+      }),
+    ],
   };
   return webpackConfig;
 };
