8a987830f2a2e907125f7aae35072e7941b8efee
Ayush Srivastava <ayushs.1992@gmail.com>
2023-06-07 14:51:47 +0000

    Update 3 files

--- client/app.js
+++ client/app.js
@@ -1,19 +1,29 @@
 import React, { useEffect } from "react";
+import { io } from "socket.io-client";
+
+const socket = io();
 
 function App() {
   useEffect(() => {
     const local = document.getElementById("video");
 
-    // display local video
-    // navigator.mediaDevices
-    //   .getUserMedia({ video: true, audio: true })
-    //   .then(function (stream) {
-    //     local.srcObject = stream;
-    //     local.play();
-    //   })
-    //   .catch(function (err) {
-    //     console.log(err);
-    //   });
+    //display local video
+    navigator.mediaDevices
+      .getUserMedia({ video: true, audio: true })
+      .then(function (stream) {
+        local.srcObject = stream;
+        //local.play();
+        socket.emit("stream", stream);
+      })
+      .catch(function (err) {
+        console.log(err);
+      });
+      
+      socket.on("stream", function(stream){
+      //remote.src = window.URL.createObjectURL(stream);
+      console.log("remote")
+
+    });
 
     return () => {};
   }, []);
